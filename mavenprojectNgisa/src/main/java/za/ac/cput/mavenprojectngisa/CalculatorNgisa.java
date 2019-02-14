4ccd39cf5a498dcb30721995890840d61e1a783a
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-02-14 02:31:19 +0200

    the beninging

---
+++ mavenprojectNgisa/src/main/java/za/ac/cput/mavenprojectngisa/CalculatorNgisa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.mavenprojectngisa;
+
+/**
+ *
+ * @author Lungisa
+ */
+public class CalculatorNgisa {
+    
+   public int  Add(int q,int b){
+   int ans = q+b; 
+   return ans;
+    }
+    
+}
